4f9812412a72a7514e9bea708f9a865c29d8aff2
Marina Grigoreva <mgrigoreva12@gmail.com>
2023-10-08 18:25:09 +0300

    Added first routes

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello/{name}', static function (string $name): string {
-    return "hello, ${name}";
+Route::get('/welcome', function (): string {
+    return 'Hi there! Welcome to my first laravel project';
+});
+
+Route::get('/info', static function (): string {
+    return 'Here will be soon a news aggregator!';
+});
+
+Route::get('/news', static function (): string {
+    return 'Page with all the news';
+});
+
+Route::get('/news/{id}', static function (int $id): string {
+    return "Welcome to page of the news with id {$id}";
 });
